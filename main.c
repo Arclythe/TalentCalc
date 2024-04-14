8620930bd655faa57bc5f366c899b7fa49a8365d
Arclythe <83001397+Arclythe@users.noreply.github.com>
2024-04-14 18:16:08 +0200

    v1.3
    added auto-restart function

--- main.c
+++ main.c
@@ -3,85 +3,95 @@
 #include <conio.h>
 #include "functions.h"
 
+#define clrscr() printf("\e[1;1H\e[2J")
+
 int main(){
-    //start here
-    printf("Genshin Mats Calculator by Maverick B-)\nRelease 14.04.2024\n\n");
 
+    while(1){
+        //start here
+        printf("Genshin Mats Calculator by Maverick\n\n");
+
+        
+        //error handler here
+        printf("Enter the level of your chara's NA: ");
+        int naStart = 1;
+        checkInput(&naStart);
     
-    //error handler here
-    printf("Enter the level of your chara's NA: ");
-    int naStart = 1;
-    checkInput(&naStart);
-   
-    // printf("naStart is: %d\n",naStart);
+        // printf("naStart is: %d\n",naStart);
 
-    //error handler here
-    printf("Enter the level of your chara's E: ");
-    int eStart = 1;
-    checkInput(&eStart);
+        //error handler here
+        printf("Enter the level of your chara's E: ");
+        int eStart = 1;
+        checkInput(&eStart);
 
-    //error handler here
-    printf("Enter the level of your chara's Q: ");
-    int qStart = 1;
-    checkInput(&qStart);
+        //error handler here
+        printf("Enter the level of your chara's Q: ");
+        int qStart = 1;
+        checkInput(&qStart);
 
-    printf("\n\n");
-    
-    //error handler here
-    printf("Enter the level of your desired chara's NA: ");
-    int naEnd = 1;
-    checkInput(&naEnd);
+        printf("\n\n");
+        
+        //error handler here
+        printf("Enter the level of your desired chara's NA: ");
+        int naEnd = 1;
+        checkInput(&naEnd);
 
-    printf("Enter the level of your desired chara's E: ");
-    int eEnd = 1;
-    checkInput(&eEnd);
+        printf("Enter the level of your desired chara's E: ");
+        int eEnd = 1;
+        checkInput(&eEnd);
 
-    printf("Enter the level of your desired chara's Q: ");
-    int qEnd = 1;
-    checkInput(&qEnd);
+        printf("Enter the level of your desired chara's Q: ");
+        int qEnd = 1;
+        checkInput(&qEnd);
 
-    printf("\n");
+        printf("\n");
 
-    Booksset * naBooks = createBookSet();
-    naBooks = evolve(naStart,naEnd);
+        Booksset * naBooks = createBookSet();
+        naBooks = evolve(naStart,naEnd);
 
-    Booksset * eBooks = createBookSet();
-    eBooks = evolve(eStart,eEnd);
+        Booksset * eBooks = createBookSet();
+        eBooks = evolve(eStart,eEnd);
 
-    Booksset * qBooks = createBookSet();
-    qBooks = evolve(qStart,qEnd);
+        Booksset * qBooks = createBookSet();
+        qBooks = evolve(qStart,qEnd);
 
-    Booksset * totalBooks = createBookSet();
-    totalBooks = combineBooks(naBooks, eBooks, qBooks);
+        Booksset * totalBooks = createBookSet();
+        totalBooks = combineBooks(naBooks, eBooks, qBooks);
 
-    printf("\n");
+        printf("\n");
 
-    printf("So this is your plan, check if everything is in order:\n");
-    printf("Normal Attack:  lvl.%2d -> %2d\n",naStart,naEnd);
-    printf("Elemental Skill:lvl.%2d -> %2d\n",eStart,eEnd);
-    printf("Burst Skill:    lvl.%2d -> %2d\n",qStart,qEnd);
+        printf("So this is your plan, check if everything is in order:\n");
+        printf("Normal Attack:  lvl.%2d -> %2d\n",naStart,naEnd);
+        printf("Elemental Skill:lvl.%2d -> %2d\n",eStart,eEnd);
+        printf("Burst Skill:    lvl.%2d -> %2d\n",qStart,qEnd);
 
-    
-    printf("\nSooo, the required books are: \n");
-    printBooks(totalBooks);
-    printf("\n\n");
+        
+        printf("\nSooo, the required books are: \n");
+        printBooks(totalBooks);
+        printf("\n\n");
 
-    Booksset * ownedBooks = createBookSet();
-    
-    printf("Enter how many books do you have (2 star): ");
-    scanf("%d",&ownedBooks->common);
-    printf("Enter how many books do you have (3 star): ");
-    scanf("%d",&ownedBooks->rare);
-    printf("Enter how many books do you have (4 star): ");
-    scanf("%d",&ownedBooks->epic);
+        Booksset * ownedBooks = createBookSet();
+        
+        printf("Enter how many books do you have (2 star): ");
+        scanf("%d",&ownedBooks->common);
+        printf("Enter how many books do you have (3 star): ");
+        scanf("%d",&ownedBooks->rare);
+        printf("Enter how many books do you have (4 star): ");
+        scanf("%d",&ownedBooks->epic);
 
-    printf("\n\n");
+        printf("\n\n");
 
-    calculate(totalBooks,ownedBooks);
+        calculate(totalBooks,ownedBooks);
 
-    printf("\n");
+        printf("\n\n\n\n\n");
 
-    printf("Press any key to exit...");
-    getch();
-    
+        printf("Press any key to restart...");
+        getch();
+
+        
+
+        
+        clrscr();
+
+    }
 }
